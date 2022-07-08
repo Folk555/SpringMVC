1a2899b36c26e5ff68e56800681e2a25601bea17
Folk555 <sanya.turulin.98@list.ru>
2022-07-08 11:14:08 +0300

    Изучена аннотация @RequestParam

--- src/main/java/ru/turulin/SpringMVC/Controllers/FirstController.java
+++ src/main/java/ru/turulin/SpringMVC/Controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.turulin.SpringMVC.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first") // дополнительный префикс в маппинге.
@@ -10,11 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    //HttpServletRequest request содержит все сведения об http запросе
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        System.out.println("Hello, " + name + " " + surname);
+//        return "first/hello";
+//    }
+
+    //@RequestParam("name") требует нужный параметр в http запросе.
+    //В случае если таких параметров нет, генерируется ошибка.
+    //Для избежания такой используют required = false
+    //Данный способ более предпочтителен, если кроме параметров ничего нет.
+
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodBye")
     public String goodByePage() {
+
         return "first/goodBye";
     }
 }
